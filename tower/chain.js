bcc5fc0114212fb062ce3d82608f541644d52c43
PuraMarpu <72298201+PuraMarpu@users.noreply.github.com>
2020-10-26 13:22:27 +0530

    Add files via upload

---
+++ tower/chain.js
@@ -0,0 +1,17 @@
+class sling{
+    constructor(bodyA,pointB){
+        var op={
+          bodyA:bodyA,
+          pointB:pointB,
+          length:160,
+          stiffness:0.06
+      }
+      this.rope = bind.create(op);
+      this.pointB = pointB;
+  
+      World.add(world,this.rope);
+    }
+    detach(){
+      this.rope.bodyA = null;
+    }
+  }
---
+++ tower/ground.js
@@ -0,0 +1,19 @@
+class Ground{
+    constructor(x,y,width,height){
+      var op ={
+         isStatic:true
+      }
+      this.terra = Bodies.rectangle(x,y,width,height,op);
+      this.width = width;
+      this.height = height;
+
+      World.add(world,this.terra);
+    }
+    earth(){
+      var pos = this.terra.position;
+
+      rectMode(CENTER);
+      fill("blue");
+      rect(pos.x,pos.y,this.width,this.height);
+    }
+}
